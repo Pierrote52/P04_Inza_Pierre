9560bf8845a0108b247103df8b4b95fb1cde3fff
Pierre Inza <pierreinza@hotmail.fr>
2021-10-05 10:49:10 +0200

    Commentaire des elements

--- modal.js
+++ modal.js
@@ -75,6 +75,7 @@ closeModal.addEventListener('click', function(){
 //fonction qui permet de savoir si le prénom ne comporte pas de numéros, commence par une majuscule et est en un seul mot. 
 document.getElementById('first').addEventListener('change',function(value){
  
+  //Expression regulière qui contrôle la saisie de texte
   if (/^[A-Za-zéèàç\-]+$/.test(value.target.value) ){
     console.log('Name Ok ! ');
     console.log(value.target.value);
@@ -83,10 +84,9 @@ document.getElementById('first').addEventListener('change',function(value){
     responseFirst.innerText= "Prénom valide";
     responseFirst.style.color="green";
     
+    //Si la Regex retourne une valeur false, je signale à l'utilisateur avec un message en Rouge en dessous du champ. 
   }else{
     lastName = null;
-    console.log("ERROR");
-    console.log(firstName);
     responseFirst.style.display= "block";
     responseFirst.innerText= "Prénom non-valide";
     responseFirst.style.color="red";
@@ -100,6 +100,8 @@ document.getElementById('first').addEventListener('change',function(value){
 //fonction qui permet de controller le NOM.
 document.getElementById('last').addEventListener('change',function(value){
  
+
+  //RegEx qui permet de controler que le champ saisie est bien un champ texte. 
   if (/^[A-Za-zéèàç\-]+$/.test(value.target.value) ){
     console.log('Name Ok ! ');
     console.log(value.target.value);
@@ -108,10 +110,9 @@ document.getElementById('last').addEventListener('change',function(value){
     responseLast.innerText= "Nom valide";
     responseLast.style.color="green";
     
+    //Ce qu'il faut faire si l'input n'est pas valide. Je notifie l'utilisateur par un petit message en dessous du champ. 
   }else{
     firstName = null;
-    console.log("ERROR");
-    console.log(lastName);
     responseLast.style.display= "block";
     responseLast.innerText= "Nom non-valide";
     responseLast.style.color="red";
@@ -121,7 +122,7 @@ document.getElementById('last').addEventListener('change',function(value){
 })
 //fonction de verification de l'email avec RegEx.
 document.getElementById('email').addEventListener('change',function(mail){
-
+//Cette RegEx permet de contrôlr la saisie d'un e-mail. 
   if(/^([a-z]||[0-9]||[-|.|/|=|+|,|?|è|é|"|'|(|&|$|*)])+[@]{1}([a-z]||[0-9]||[-|.|/|=|+|,|?|è|é|"|'|(|&|$|*)]){3,}[.]{1}[a-z]{2,3}$/.test(mail.target.value)){
     responseMail.textContent = "e-mail valide";
     responseMail.style.color = "green";
@@ -129,7 +130,6 @@ document.getElementById('email').addEventListener('change',function(mail){
     email = mail;
 
   } else {
-    console.log('email non-valide');
     responseMail.textContent = "e-mail non-valide";
     responseMail.style.color = "red";
     responseMail.style.display= "block";
@@ -145,6 +145,7 @@ document.getElementById('email').addEventListener('change',function(mail){
 document.getElementById('birthdate').addEventListener('change',function(birth){
   birth.preventDefault();
 
+  //Cette RegEx permet de verifier que la date de naissance soit cohérente. 
   if(/^[1]{1}[9]{1}[1-9]{1}[0-9]{1}[-]{1}/.test(birth.target.value) || /^[2]{1}[0]{1}(([0]{1}[0-9]{1})||([1]{1}[0-4]{1}))[-]{1}/.test(birth.target.value)  ){
     // console.log(birth.target.value);
     // console.log('Date Ok ');
@@ -172,7 +173,7 @@ document.getElementById('quantity').addEventListener('input', function(quantity)
 
   console.log();
   
-
+//Verifie que le le nombre de parcours réalisés soient inférieurs à 99 , et que la lettre e ne soit pas mentionnée dans le champ. Cette lettre etant prise en compte à cause de sa signification Exponencielle "e". 
   if(quantity.data > 99 || quantity.data =="e"||quantity.data ==null){
     quantityCount = null;
     responseQuantity.textContent = "donnée non-valide";
@@ -191,7 +192,7 @@ document.getElementById('quantity').addEventListener('input', function(quantity)
 
 //fonction qui contrôle le nom de la ville et l'ajoute aux villes de participation. 
 document.getElementById('location1').addEventListener('click', function(event){
-  participationCity = event.target.value;
+  participationCitys = event.target.value;
 })
 
 //Cette fonction vérifie que les conditions d'utilisations ont bien été acceptées. 
@@ -212,7 +213,7 @@ document.getElementById('checkbox2').addEventListener('click', function(){
 //Cette fonction vérifie la validité du formulaire. 
 document.getElementById('submit').addEventListener('click', function(elemnt){
       elemnt.preventDefault();
-
+//Ici nous vérifions que tous les champs soient correctement remplies. 
   if(lastName!=null && firstName!=null && email!=null && birthDay!=null && acceptConditionsUtilisation==true){
     console.log('Formulaire OK ');
     let form = document.getElementById('formulaire');
